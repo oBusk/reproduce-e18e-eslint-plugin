34e6ced7b646da3b138ab33af51e43238437b357
Oscar Busk <oscar.busk@gmail.com>
2026-03-23 14:39:46 +0100

    Reproduce e18e/eslint-plugin#90

--- repro.ts
+++ repro.ts
@@ -1,1 +1,13 @@
-// Put some ts that breaks linting here
+const a = [1, 2, 3];
+const bArray = [4, 5, 6];
+const bValue = 4;
+
+let result;
+
+result = a.concat(bArray);
+// With type information becomes: [...a, bArray] ❌
+// Without type information becomes: [...a, ...bArray] ✅
+
+result = a.concat(bValue);
+// With type information becomes: [...a, bValue] ✅
+// Without type information becomes: [...a, ...bValue] ❌
